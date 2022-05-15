ea1cb3798dcc88e45d2afc9504c8bb0a45381a6a
Duong Vu <thien.vu97ht123@gmail.com>
2022-05-16 01:39:57 +0700

    63. Cấu hình Router/Layout cho dự án

--- src/App.js
+++ src/App.js
@@ -1,8 +1,39 @@
+import { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { DefaultLayout } from '~/components/Layouts';
+import { publicRoutes } from '~/routes';
+
 function App() {
     return (
-        <div className="App">
-            <h1>React App</h1>
-        </div>
+        <Router>
+            <div className="App">
+                <Routes>
+                    {publicRoutes?.map((route, index) => {
+                        const Page = route.component;
+
+                        let Layout = DefaultLayout;
+
+                        if (route.layout) {
+                            Layout = route.layout;
+                        } else if (route.layout === null) {
+                            Layout = Fragment;
+                        }
+
+                        return (
+                            <Route
+                                key={index}
+                                path={route.path}
+                                element={
+                                    <Layout>
+                                        <Page />
+                                    </Layout>
+                                }
+                            />
+                        );
+                    })}
+                </Routes>
+            </div>
+        </Router>
     );
 }
 
---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import { HeaderOnly } from '~/components/Layouts';
+import Following from '~/pages/Following';
+import Home from '~/pages/Home';
+import Profile from '~/pages/Profile';
+import Search from '~/pages/Search';
+import Upload from '~/pages/Upload';
+
+// Public routes
+const publicRoutes = [
+    { path: '/', component: Home },
+    { path: '/following', component: Following },
+    { path: '/profile', component: Profile },
+    { path: '/upload', component: Upload, layout: HeaderOnly },
+    { path: '/search', component: Search, layout: null },
+];
+
+const privateRoutes = [];
+
+export { publicRoutes, privateRoutes };
