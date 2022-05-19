e84c67a216697f410c29fcba21d732adbc2fe275
Duong Vu <thien.vu97ht123@gmail.com>
2022-05-19 23:03:08 +0700

    64. Dựng khung Layout mặc định

--- src/components/Layouts/DefaultLayout/Sidebar/index.js
+++ src/components/Layouts/DefaultLayout/Sidebar/index.js
@@ -1,7 +1,15 @@
+import classNames from 'classnames/bind';
 import React from 'react';
+import styles from './Sidebar.module.scss';
+
+const cx = classNames.bind(styles);
 
 function Sidebar() {
-    return <h2>Sidebar</h2>;
+    return (
+        <aside className={cx('wrapper')}>
+            <h2>Sidebar</h2>
+        </aside>
+    );
 }
 
 export default Sidebar;
