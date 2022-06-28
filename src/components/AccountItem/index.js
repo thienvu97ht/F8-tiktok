2b9c56857477e66c6ca99ede32313e3b31328e47
Vũ nè <thien.vu97ht123@gmail.com>
2022-06-29 01:50:52 +0700

    Tìm Hiểu Thư Viện PropTypes Trong React

--- src/components/AccountItem/index.js
+++ src/components/AccountItem/index.js
@@ -1,7 +1,7 @@
 import { faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import classNames from 'classnames/bind';
-import React from 'react';
+import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Image from '../Image';
 import styles from './AccountItem.module.scss';
@@ -22,5 +22,9 @@ function AccountItem({ data }) {
         </Link>
     );
 }
+
+AccountItem.propTypes = {
+    data: PropTypes.object,
+};
 
 export default AccountItem;
