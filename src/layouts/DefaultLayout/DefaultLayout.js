1945fbd3f5bce75c0ea97b1702c1ecfa9baf5f9f
Vũ nè <thien.vu97ht123@gmail.com>
2022-07-09 23:55:13 +0700

    Xây Dựng UI Phần Sidebar #1

--- src/layouts/DefaultLayout/DefaultLayout.js
+++ src/layouts/DefaultLayout/DefaultLayout.js
@@ -1,9 +1,8 @@
-import React from 'react';
-import Header from '../components/Header/Header';
-import Sidebar from './Sidebar/Sidebar';
-import styles from './DefaultLayout.module.scss';
 import classNames from 'classnames/bind';
 import PropTypes from 'prop-types';
+import Header from '../components/Header/Header';
+import Sidebar from '../components/Sidebar';
+import styles from './DefaultLayout.module.scss';
 
 const cx = classNames.bind(styles);
 
---
+++ src/pages/Live/Live.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Live() {
+    return <h2>Live</h2>;
+}
+
+export default Live;
